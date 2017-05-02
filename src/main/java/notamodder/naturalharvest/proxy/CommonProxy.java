dc6c479fa6a1af2fa81a77b6b3e3049f6c98852f
NotAModder <modderiamnot@gmail.com>
2017-05-01 20:57:02 -0600

    Added jellyfish to the oredict.

--- src/main/java/notamodder/naturalharvest/proxy/CommonProxy.java
+++ src/main/java/notamodder/naturalharvest/proxy/CommonProxy.java
@@ -3,8 +3,10 @@ package notamodder.naturalharvest.proxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
 import notamodder.naturalharvest.NaturalHarvest;
 import notamodder.naturalharvest.data.HarvestItems;
+import notamodder.notalib.utils.*;
 
 /**
  * This class contains code which is meant to be executed on both the client and server. Any
@@ -14,9 +16,11 @@ public class CommonProxy {
 
     public void preInit (FMLPreInitializationEvent event) {
 
+        // Item Registry
         NaturalHarvest.registry.registerItem(HarvestItems.JELLYFISH, "jellyfish");
-
-        // OreDictionary.registerOre("", ore);
+        
+        // OreDict
+        OreDictionary.registerOre(OreDictEntries.SLIMEBALL, HarvestItems.JELLYFISH);
     }
 
     public void init (FMLInitializationEvent event) {
