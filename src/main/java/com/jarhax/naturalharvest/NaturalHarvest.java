3aac601b8b65bb536e4c5f8e88001c3c6cfa7bb2
Tyler Hancock <darklime@live.ca>
2017-09-05 22:51:45 -0600

    Cleaned up main mod class again.

--- src/main/java/com/jarhax/naturalharvest/NaturalHarvest.java
+++ src/main/java/com/jarhax/naturalharvest/NaturalHarvest.java
@@ -6,8 +6,8 @@ import static com.jarhax.naturalharvest.NaturalHarvest.NAME;
 import com.jarhax.naturalharvest.proxy.CommonProxy;
 import com.jarhax.naturalharvest.registry.NaturalHarvestRegistries;
 
+import net.darkhax.bookshelf.lib.LoggingHelper;
 import net.darkhax.bookshelf.registry.RegistryHelper;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -19,6 +19,8 @@ public class NaturalHarvest {
 
     public static final String MODID = "naturalharvest";
     public static final String NAME = "Natural Harvest";
+    public static final LoggingHelper LOG = new LoggingHelper(NAME);
+    public static final RegistryHelper registry = new RegistryHelper(MODID);
 
     @Mod.Instance(MODID)
     public static NaturalHarvest INSTANCE;
@@ -26,14 +28,11 @@ public class NaturalHarvest {
     @SidedProxy(clientSide = "com.jarhax.naturalharvest.proxy.ClientProxy", serverSide = "com.jarhax.naturalharvest.proxy.ServerProxy")
     public static CommonProxy proxy;
 
-    public static RegistryHelper registry = new RegistryHelper(MODID);
-
     @Mod.EventHandler
     public void preInit (FMLPreInitializationEvent event) {
 
-        proxy.preInit(event);
         NaturalHarvestRegistries.init();
-        MinecraftForge.EVENT_BUS.register(this);
+        proxy.preInit(event);
     }
 
     @Mod.EventHandler
